758f207622f2fbc34bdbaac65c5ac61e09a78347
Brandon <brandon101morgan@outlook.com>
2022-10-05 12:39:55 -0500

    Nmap scan whole wifi network

---
+++ nmap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+sudo nmap -sn 127.0.0.1/24 #this will scan all ports on your home network and display them. 
+# use the command ip addr or ifconfig to get the inet ip starting with 192. 
